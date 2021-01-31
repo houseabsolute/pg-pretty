65d19581c90c51aafe6bb32099cd17ce49c3f0b6
Dave Rolsky <autarch@urth.org>
2021-01-30 22:07:40 -0600

    Add omegasort for .gitignore and force clippy in precious.toml

---
+++ dev/bin/force-clippy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+# This seems to be just enough to force a recompilation, so clippy is actually
+# executed. But it doesn't require rebuilding every dep, so it's pretty fast.
+rm -fr target/debug/deps/*bitflags_serde_int*
+rm -fr target/debug/deps/*pretty_pg*
+rm -fr target/debug/deps/*pg_pretty*
+cargo clippy --all-targets --all-features -- -D clippy::all
